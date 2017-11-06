86c47016431c61415a54ee7a23a3c3acd8fadda9
Floris van Vugt <f.t.vanvugt@gmail.com>
2017-11-06 17:48:57 -0500

    Fiddling with plotting the delay detection results.

--- analysis/explore_delaydetect.R
+++ analysis/explore_delaydetect.R
@@ -1,6 +1,8 @@
 
 
-dat <- read.table('robert_delaydetection_20171106_120315.txt',header=T)
+fname <- 'Robert_delaydetection_20171106_162230.txt'
+##fname <- 'Thomas_delaydetection_20171106_151624.txt'
+dat <- read.table(fname,header=T)
 
 
 dat$correct.answer <- ifelse(dat$delay1==0,"second","first")
@@ -10,11 +12,22 @@ dat$correct <- dat$correct.answer==dat$response
 dat$delay <- dat$delay1+dat$delay2
 
 require('plyr')
-summ <- ddply(dat,'delay',summarize,correct=mean(correct))      
+summ <- ddply(dat,'delay',summarize,
+              n.correct=sum(correct),
+              n = length(correct))
 
+summ$prop.correct=summ$n.correct/summ$n
+summ
 
 require('ggplot2')
 
 ggplot(summ,
-       aes(x=delay,y=correct))+
-    geom_bar(stat='identity')
+       aes(x=delay,y=prop.correct))+
+    geom_point()+
+    geom_line()+
+    ggtitle(fname)+
+    xlab("Delay (ms)")+
+    ylab("Proportion correct")
+ggsave(paste(fname,".pdf",sep=''))
+
+
